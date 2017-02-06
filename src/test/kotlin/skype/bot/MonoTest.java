d60ec2ce458ceeb771762ac22a434ca2e1ee2468
Yevhenii Melnyk <melnyk.yevhenii@gmail.com>
2017-02-06 20:50:37 +0200

    Use otherwise with retry

--- src/test/kotlin/skype/bot/MonoTest.java
+++ src/test/kotlin/skype/bot/MonoTest.java
@@ -3,6 +3,8 @@ package skype.bot;
 import org.junit.Test;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
@@ -11,7 +13,9 @@ public class MonoTest {
 	@Test
 	public void shouldRetryCachedError() {
 		SampleService sampleService = new SampleService();
-		Mono<String> producer = Mono.fromSupplier(sampleService::call).cache();
+
+		Mono<String> cachedMono = Mono.fromSupplier(sampleService::call).cache();
+		Mono<String> producer = Mono.defer(() -> cachedMono);
 		try {
 			producer.block();
 			fail();
@@ -19,8 +23,16 @@ public class MonoTest {
 			System.out.println("Caught exception : " + e);
 		}
 
-		sampleService.serverAvailable = true;
-		String result = producer.retry(1, t -> {
+		AtomicInteger i = new AtomicInteger(0);
+		String result = producer.otherwise(e -> {
+			if (i.get() == 3) {
+				sampleService.serverAvailable = true;
+			} else {
+				i.set(i.get() + 1);
+			}
+			return Mono.just(sampleService.call());
+		}).retry(5, t -> {
+			System.out.println("THROWABLE " + t);
 			System.out.println("ASKING FOR RETRY");
 			return true;
 		}).block();
